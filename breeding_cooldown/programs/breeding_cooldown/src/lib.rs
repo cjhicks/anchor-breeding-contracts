42a701f800af27cc521990cd74750edeb6a37ff5
cjhicks <hicksch@gmail.com>
2021-12-28 22:47:42 -0600

    included counter example with tests to get started

--- breeding_cooldown/programs/breeding_cooldown/src/lib.rs
+++ breeding_cooldown/programs/breeding_cooldown/src/lib.rs
@@ -5,10 +5,39 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod breeding_cooldown {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
--- breeding_cooldown/tests/breeding_cooldown.ts
+++ breeding_cooldown/tests/breeding_cooldown.ts
@@ -1,17 +1,70 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { BreedingCooldown } from '../target/types/breeding_cooldown';
+const { SystemProgram } = anchor.web3;
+const assert = require("assert");
 
 describe('breeding_cooldown', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.BreedingCooldown as Program<BreedingCooldown>;
+  const wallet = anchor.getProvider().wallet;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const counter = anchor.web3.Keypair.generate();
+
+  const program = (<any>anchor).workspace.BreedingCooldown as Program<BreedingCooldown>;
+
+  it('Initializes counter to 0', async () => {
+    await program.rpc.initialize(wallet.publicKey, {
+      accounts: {
+        counter: counter.publicKey,
+        user: wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [counter]
+    })
+    
+    let counterAccount = await program.account.counter.fetch(counter.publicKey)
+
+    assert(counterAccount.authority.equals(wallet.publicKey))
+    assert(counterAccount.count.toNumber() == 0)
   });
+
+  it('Increments counter', async () => {
+    await program.rpc.increment({
+      accounts: {
+        counter: counter.publicKey,
+        authority: wallet.publicKey
+      }
+    })
+
+    let counterAccount = await program.account.counter.fetch(counter.publicKey)
+
+    assert(counterAccount.authority.equals(wallet.publicKey))
+    assert(counterAccount.count.toNumber() == 1)
+  });
+
+  it('Throws error if wallet is not authorized to increment', async () => {
+    let wrongWalletPubKey = anchor.web3.Keypair.generate();
+
+    let promise = program.rpc.increment({
+      accounts: {
+        counter: counter.publicKey,
+        authority: wrongWalletPubKey
+      }
+    })
+
+    try {
+      await promise
+    } catch (error) {
+      assert((<string>error.message) == 'Wrong input type for account "authority" in the instruction accounts object for instruction "increment". Expected PublicKey or string.')
+    }
+
+    // assert counter left unchanged
+    let counterAccount = await program.account.counter.fetch(counter.publicKey)
+    assert(counterAccount.authority.equals(wallet.publicKey))
+    assert(counterAccount.count.toNumber() == 1)
+  });
+
 });
