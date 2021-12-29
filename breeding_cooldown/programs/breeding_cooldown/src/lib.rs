00460443ce71ff9b40e5114ed5dc6bbd9466213d
cjhicks <hicksch@gmail.com>
2021-12-29 00:15:28 -0600

    start adding cooldown logic

--- breeding_cooldown/programs/breeding_cooldown/src/lib.rs
+++ breeding_cooldown/programs/breeding_cooldown/src/lib.rs
@@ -6,38 +6,65 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod breeding_cooldown {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> ProgramResult {
-        let counter = &mut ctx.accounts.counter;
-        counter.authority = authority;
-        counter.count = 0;
+    pub fn create_potion(ctx: Context<CreatePotion>, authority: Pubkey) -> ProgramResult {
+        let potion = &mut ctx.accounts.potion;
+        potion.authority = authority;
+        // TODO: should price be here? should we charge to create the potion too?
+        potion.price = 5;
+        potion.cooldown_days = 7; // days, could be parameterized
+        let timestamp = Clock::get()?.unix_timestamp as u64;
+        potion.created_timestamp = timestamp;
+
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
-        let counter = &mut ctx.accounts.counter;
-        counter.count += 1;
+    pub fn react(ctx: Context<React>) -> ProgramResult {
+
+        let potion = &mut ctx.accounts.potion;
+
+        let day_to_seconds = 60 * 60 * 24;
+        let cooldown_expiration = potion.created_timestamp + (potion.cooldown_days * day_to_seconds);
+        let timestamp = Clock::get()?.unix_timestamp as u64;
+
+        if cooldown_expiration > timestamp {
+            return Err(ErrorCode::CooldownNotReached.into());
+        }
+
+        // TODO: mint new NFT
+        // TODO: optionally, do fast reaction
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 40)]
-    pub counter: Account<'info, Counter>,
+pub struct CreatePotion<'info> {
+    #[account(init, payer = user, space = 8 + 80)]
+    pub potion: Account<'info, Potion>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Increment<'info> {
+pub struct React<'info> {
     #[account(mut, has_one = authority)]
-    pub counter: Account<'info, Counter>,
+    pub potion: Account<'info, Potion>,
     pub authority: Signer<'info>,
 }
 
 #[account]
-pub struct Counter {
+pub struct Potion {
     pub authority: Pubkey,
-    pub count: u64,
+    pub price: u64,
+    pub cooldown_days: u64,
+    pub created_timestamp: u64
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("This potion has already been created.")]
+    AlreadyCreated,
+    #[msg("This potion has not reached its cooldown period.")]
+    CooldownNotReached,
 }
