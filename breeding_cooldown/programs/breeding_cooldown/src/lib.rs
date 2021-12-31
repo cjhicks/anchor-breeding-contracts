f6dccc8e0bfe668ec041a397ff9123a2915bf70f
cjhicks <hicksch@gmail.com>
2021-12-31 11:23:01 -0600

    WIP - using anchor-spl for token instructions

--- breeding_cooldown/programs/breeding_cooldown/src/lib.rs
+++ breeding_cooldown/programs/breeding_cooldown/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl;
 use spl_token;
 use solana_program;
 
@@ -8,54 +9,64 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod breeding_cooldown {
     use super::*;
 
-    /*
-    Inputs:
-    - Potion Account
-    - Payer Account (also signer)
-    - Payer Bape Wallet (account + token)??
-    - $BAPE token program (owner of mint)
-    - $BAPE token mint
-
-    Steps:
-
-    Outputs:
-    - Created Potion Account from token (owned by program)
-    - Burned xxx BAPE (5% inflation per 30 days?)
-    */
     pub fn create_potion(ctx: Context<CreatePotion>, authority: Pubkey) -> ProgramResult {
         let potion = &mut ctx.accounts.potion;
-        let bape_token_program = &ctx.accounts.bape_token_program;
+        let token_program = &ctx.accounts.token_program;
         let user = &ctx.accounts.user;
-        let user_bape_wallet = &ctx.accounts.user_bape_wallet;
-        let bape_token_mint = &ctx.accounts.bape_token_mint;
+        let user_token_account = ctx.accounts.user_token_account.to_account_info();
+        let token_mint = ctx.accounts.token_mint.to_account_info();
 
         potion.authority = authority;
 
+        // TODO: mark apes as "used" (need another state account for this?)
+        // TODO: check if 7 days since done this...
+        // Do we have an existing token metadata account for the ape?
+        // If so, update with timestamp (need to handle secondary sale case though)
+        // Otherwise, create before coming into this method?
+        // Like this? https://github.com/LexBrill/SolNFT/blob/master/src/processor.rs#L72
+
         // TODO: check that user has enough $BAPE to get a potion
         let burn_price = 350; // TODO: inflation?
-
-        let burn_bape = spl_token::instruction::burn(
-            bape_token_program.key,
-            user_bape_wallet.key,
-            bape_token_mint.key,
-            user.key,
-            &[&user.key],
-            burn_price
-        )?;
-        solana_program::program::invoke(
-            &burn_bape,
-            &[
-                user_bape_wallet.to_account_info(),
-                user.to_account_info(),
-                bape_token_program.to_account_info(),
-            ]
-        )?;
-
-        // TODO: mark apes as "used" (need another state account for this?)
+        let cpi_accounts = anchor_spl::token::Burn {
+            to: user_token_account,
+            mint: token_mint,
+            authority: user.to_account_info(),
+        };
+        let cpi_program = token_program.clone();
+        let burn_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        anchor_spl::token::burn(burn_ctx, burn_price).expect("burn failed.");
 
         // TODO: mint new NFT for potion
+        // anchor.web3.SystemProgram.createAccount({
+        //     fromPubkey: walletKey,
+        //     newAccountPubkey: eggMint,
+        //     space: MintLayout.span,
+        //     lamports: rent,
+        //     programId: TOKEN_PROGRAM_ID,
+        //   }),
+        //   Token.createInitMintInstruction(
+        //     TOKEN_PROGRAM_ID,
+        //     eggMint,
+        //     0,
+        //     walletKey,
+        //     walletKey
+        //   ),
+        //   createAssociatedTokenAccountInstruction(
+        //     eggToken,
+        //     walletKey,
+        //     walletKey,
+        //     eggMint
+        //   ),
+        //   Token.createMintToInstruction(
+        //     TOKEN_PROGRAM_ID,
+        //     eggMint,
+        //     eggToken,
+        //     walletKey,
+        //     [],
+        //     1
+        //   ),
+        // anchor_spl::token::mint_to(), amount: u64)
 
-        // TODO: should price be here? should we charge to create the potion too?
         potion.fast_react_price = 5;
         potion.cooldown_days = 7; // days, could be parameterized
         let timestamp = Clock::get()?.unix_timestamp as u64;
@@ -101,9 +112,10 @@ pub struct CreatePotion<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(mut)]
-    pub user_bape_wallet: Signer<'info>,
-    pub bape_token_program: Program<'info, System>,
-    pub bape_token_mint: Program<'info, System>,
+    pub user_token_account: AccountInfo<'info>,  // User's $BAPE (or generic token) account, this token type should match mint account
+    #[account(mut)]
+    pub token_mint: AccountInfo<'info>,  // $BAPE mint, generic enough for any token though
+    pub token_program: AccountInfo<'info>,  // this is the SPL Token Program which is owner of all token mints
     pub system_program: Program<'info, System>,
 }
 
@@ -119,8 +131,11 @@ pub struct FastReact<'info> {
     #[account(mut, has_one = authority)]
     pub potion: Account<'info, Potion>,
     pub authority: Signer<'info>,
-    pub bape_token_program: Program<'info, System>,
-    pub bape_token_mint: Program<'info, System>,
+    #[account(mut)]
+    pub user_token_account: Signer<'info>,  // User's $BAPE (or generic token) account, this token type should match mint account
+    #[account(mut)]
+    pub token_mint: AccountInfo<'info>,  // $BAPE mint, generic enough for any token though
+    pub token_program: AccountInfo<'info>,  // this is the SPL Token Program which is owner of all token mints
 }
 
 #[account]
