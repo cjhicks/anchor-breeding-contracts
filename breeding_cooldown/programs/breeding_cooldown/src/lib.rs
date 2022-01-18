7933b998f64bcd7e3cfa2c228f25ed6bce8b924a
cjhicks <hicksch@gmail.com>
2022-01-17 23:31:42 -0600

    metadata with master edition

--- breeding_cooldown/programs/breeding_cooldown/src/lib.rs
+++ breeding_cooldown/programs/breeding_cooldown/src/lib.rs
@@ -1,13 +1,13 @@
 use anchor_lang::prelude::*;
 use anchor_spl;
 use solana_program::{
-    sysvar::{clock::Clock}
+    sysvar::{clock::Clock},
+    sysvar
 };
-use solana_program::program::{invoke_signed, invoke};
-#[allow(unused_imports)]
+use solana_program::program::{invoke_signed};
 use spl_token_metadata::{
-        instruction::{update_metadata_accounts, create_metadata_accounts, CreateMasterEditionArgs, MetadataInstruction},
-        state::{Creator, Data, Metadata, MAX_URI_LENGTH, MAX_NAME_LENGTH},
+        instruction::{update_metadata_accounts, CreateMetadataAccountArgs, CreateMasterEditionArgs, MetadataInstruction}, //create_metadata_accounts
+        state::{Creator, Data},
 };
 use solana_program::instruction::{Instruction,AccountMeta};
 
@@ -22,7 +22,7 @@ fn get_breed_min_timestamp(timestamp: u64) -> u64 {
     return timestamp - seven_days_in_seconds;
 }
 
-const PREFIX: &str = "bapeBreedingTest16";
+const PREFIX: &str = "bapeBreedingTest17";
 const PREFIX_POTION: &str = "potion";
 
 #[program]
@@ -33,8 +33,6 @@ pub mod breeding_cooldown {
     This function is equivalent to breeding an egg: https://explorer.solana.com/tx/g5fg51XveddE1MyU3GsEUpU6e3vUz1BhWNBvye6hBziDZbKsBv4H1UjLEKr1rjLFtABt6YNM6TBBoMzDxtQ5td5
     */
     pub fn create_potion(ctx: Context<CreatePotion>, creator_bump: u8) -> ProgramResult {
-        // let CANDY_MACHINE_ID = "6vHjQxYUwk9DNuJNHfRWSfH1UTuikVayP9h3H4iYW2TD";
-
         let potion_mint = &mut ctx.accounts.potion_mint;
         // let potion_state = &mut ctx.accounts.potion_state;
         let potion_mint_metadata = &mut ctx.accounts.potion_mint_metadata;
@@ -125,7 +123,7 @@ pub mod breeding_cooldown {
         Mint new NFT for potion
         */
         let uri = r"https://bafybeibhsnl5sz32jdfdwfvj4qea3at25wobxenzwjdirrdr2h3i4u4y2a.ipfs.infura-ipfs.io";
-        let mut creators_ptn = vec![
+        let creators_ptn = vec![
             Creator{
                 address: potion_creator.key(),
                 verified: true,
@@ -153,66 +151,62 @@ pub mod breeding_cooldown {
             true,
             true, // false?
         );
-
-        // fine until here
-        let mut temp_creator = potion_creator.clone();
-        temp_creator.is_signer = true;
-        temp_creator.is_writable = true;
         invoke_signed(
             create_metadata_ix,
             &[
                 potion_mint_metadata.clone(),
                 potion_mint.to_account_info().clone(),
-                user.to_account_info().clone(), //tmp.clone(),
-                potion_creator.clone(), //potion_creator.to_account_info().clone(),
+                user.to_account_info().clone(),
+                potion_creator.clone(),
                 token_program.to_account_info().clone(),
                 system_program.clone(),
                 rent.to_account_info().clone(),
                 token_metadata_program.to_account_info().clone()
             ],
             &[&[PREFIX.as_bytes(), PREFIX_POTION.as_bytes(), &[creator_bump]]]
-            // &[&[b"metadata", token_metadata_program.key().as_ref(), potion_mint.key().as_ref()]],
         ).expect("create_metadata_accounts failed.");
 
-        // invoke_signed(
-        //     &create_master_edition(
-        //         token_metadata_program.key(), 
-        //         potion_master_edition.key(),
-        //         potion_mint.key(),
-        //         user.key(),
-        //         user.key(),
-        //         potion_mint_metadata.key(),
-        //         user.key(),
-        //         Some(0),
-        //     ),
-        //     &[  
-        //         potion_master_edition.clone().to_account_info(),
-        //         potion_mint.clone().to_account_info(),
-        //         // price_data_info.clone().to_account_info(),
-        //         user.clone().to_account_info(),
-        //         potion_mint_metadata.clone().to_account_info(),
-        //         potion_token.clone().to_account_info(),
-        //         token_metadata_program.to_account_info()
-        //     ],
-        //     &[&[&"price".as_bytes(), &[price_bump]]],
-        // )?;
+        invoke_signed(
+            &create_master_edition(
+                token_metadata_program.key(), 
+                potion_master_edition.key(),
+                potion_mint.key(),
+                potion_creator.key(),
+                user.key(),
+                potion_mint_metadata.key(),
+                user.key(),
+                Some(0),
+            ),
+            &[  
+                potion_master_edition.clone().to_account_info(),
+                potion_mint.clone().to_account_info(),
+                potion_creator.clone(),
+                user.clone().to_account_info(),
+                potion_mint_metadata.clone().to_account_info(),
+                potion_token.clone().to_account_info(),
+                system_program.clone(),
+                rent.to_account_info().clone(),
+                token_metadata_program.to_account_info()
+            ],
+            &[&[PREFIX.as_bytes(), PREFIX_POTION.as_bytes(), &[creator_bump]]]
+        )?;
 
-        // invoke_signed(
-        //     &update_metadata_accounts(
-        //         spl_token_metadata::id(), 
-        //         potion_mint_metadata.key(),
-        //         price_data_info_key,
-        //         None,
-        //         None,
-        //         Some(true),
-        //     ),
-        //     &[  
-        //         potion_mint_metadata.clone().to_account_info(),
-        //         // price_data_info.clone(),
-        //         token_metadata_program.to_account_info()
-        //     ],
-        //     &[&[&"price".as_bytes(), &[price_bump]]],
-        // )?;
+        invoke_signed(
+            &update_metadata_accounts(
+                token_metadata_program.key(), 
+                *potion_mint_metadata.key,
+                *potion_creator.key,
+                None,
+                None,
+                Some(true),
+            ),
+            &[  
+                potion_mint_metadata.clone().to_account_info(),
+                potion_creator.clone(),
+                token_metadata_program.to_account_info()
+            ],
+            &[&[PREFIX.as_bytes(), PREFIX_POTION.as_bytes(), &[creator_bump]]]
+        )?;
 
         Ok(())
     }
@@ -326,9 +320,77 @@ pub mod breeding_cooldown {
     // }
 }
 
-#[account]
-pub struct CreatorData {
-    // pub created_timestamp: u64
+
+pub fn create_metadata_accounts(
+    program_id: Pubkey,
+    metadata_account: Pubkey,
+    mint: Pubkey,
+    mint_authority: Pubkey,
+    payer: Pubkey,
+    update_authority: Pubkey,
+    name: String,
+    symbol: String,
+    uri: String,
+    creators: Option<Vec<Creator>>,
+    seller_fee_basis_points: u16,
+    update_authority_is_signer: bool,
+    is_mutable: bool,
+) -> Instruction {
+    Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(metadata_account, false),
+            AccountMeta::new_readonly(mint, false),
+            AccountMeta::new_readonly(mint_authority, true),
+            AccountMeta::new(payer, true),
+            AccountMeta::new_readonly(update_authority, update_authority_is_signer),
+            AccountMeta::new_readonly(solana_program::system_program::id(), false),
+            AccountMeta::new_readonly(sysvar::rent::id(), false),
+        ],
+        data: MetadataInstruction::CreateMetadataAccount(CreateMetadataAccountArgs {
+            data: Data {
+                name,
+                symbol,
+                uri,
+                seller_fee_basis_points,
+                creators,
+            },
+            is_mutable,
+        })
+        .try_to_vec()
+        .unwrap(),
+    }
+}
+
+pub fn create_master_edition(
+    program_id: Pubkey,
+    edition: Pubkey,
+    mint: Pubkey,
+    update_authority: Pubkey,
+    mint_authority: Pubkey,
+    metadata: Pubkey,
+    payer: Pubkey,
+    max_supply: Option<u64>,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(edition, false),
+        AccountMeta::new(mint, false),
+        AccountMeta::new_readonly(update_authority, true),
+        AccountMeta::new_readonly(mint_authority, true),
+        AccountMeta::new(payer, true),
+        AccountMeta::new_readonly(metadata, false),
+        AccountMeta::new_readonly(spl_token::id(), false),
+        AccountMeta::new_readonly(solana_program::system_program::id(), false),
+        AccountMeta::new_readonly(sysvar::rent::id(), false),
+    ];
+
+    Instruction {
+        program_id,
+        accounts,
+        data: MetadataInstruction::CreateMasterEdition(CreateMasterEditionArgs { max_supply })
+            .try_to_vec()
+            .unwrap(),
+    }
 }
 
 // #[account]
@@ -363,7 +425,7 @@ pub struct NftMetadata {
 #[derive(Accounts)]
 #[instruction(creator_bump: u8)]
 pub struct CreatePotion<'info> {
-    #[account(mut)]
+    // #[account(mut)]
     pub user: Signer<'info>,
     #[account(mut)]
     pub potion_mint: AccountInfo<'info>,
